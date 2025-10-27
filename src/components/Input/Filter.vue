8897062bbb9b91a657460a162249d86b796f2e4f
anggarchmdi <anggarchmdi@gmail.com>
2025-10-27 13:40:04 +0700

    fix filter

--- src/components/Input/Filter.vue
+++ src/components/Input/Filter.vue
@@ -1,20 +1,29 @@
 <template>
-<div class="">
-  <input
-        :type="showPassword ? 'text' : type"
-        class="p-3.5 h-8 w-[256px] bg-[#EDF0F2] outline-none transition"
-        :placeholder="placeholder"
-        :value="modelValue"
-      />
-</div>
+  <div>
+    <input
+      :type="showPassword ? 'text' : type"
+      class="p-3.5 h-8 w-[256px] bg-[#EDF0F2] outline-none transition"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
 </template>
 
 <script>
-  export default {
-    props: ['placeholder', 'modelValue']
-  }
+export default {
+  props: {
+    placeholder: String,
+    modelValue: String,
+    type: {
+      type: String,
+      default: 'text'
+    },
+    showPassword: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ['update:modelValue']
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
